5205f458e9efc7eb26d90bcc499a3d4a158852a0
fotini <f.batzoni@gmail.com>
2023-01-11 15:46:32 +0200

    Creating ApplicationDbContext and Adding the ConnectionString in application.json

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -10,9 +10,20 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [MaxLength(30)]
         public string Name { get; set; }
 
+        public string Details { get; set; }
+
+        [Required]
+        public double Rate { get; set; }
+
+
         public int Occupancy { get; set; }
 
         //Sqft = squareFeet
         public int Sqft { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Amenity { get; set; }
+
     }
 }
